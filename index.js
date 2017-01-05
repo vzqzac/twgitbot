ccf4a3609e94c63ca4fd1cb1bb8e8638e64289ab
Omar Vazquez <omar.vazquezac@gmail.com>
2017-01-04 23:23:43 -0600

    added express to work in heroku

--- index.js
+++ index.js
@@ -3,6 +3,20 @@
 const checker = require('./Github/repoChecker')
 const twitMessager = require('./Twitter/postTwit')
 
+// In order to work with heroku I need a server so...
+const express = require('express')
+const app = express()
+
+app.all('*', function (req, res) {
+  res.send("Hello, I'm Twgitbot\nFind me on https://github.com/VzqzAc/twgitbot")
+})
+
+let port = process.env.PORT || 3000
+
+app.listen(port, function (error) {
+  if (error) console.error(error)
+})
+
 let interval
 
 try {
